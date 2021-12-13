da9c2465aa329333e9b66b95c10ef13792370a89
Sandalas98 <1811552@gmail.com>
2021-12-13 11:21:26 +0100

    Poprawiono main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import file_extract, pdf_analyse, doc_to_pdf_conv
 import os
 
 
+# Tu podać ścieżki do dysku (lub folderu z wypakowanym obrazem dyskowym):
 file_extract.extract_files('./mount')
-doc_to_pdf_conv.conv_2_pdf('./analysis_dir/docs/')
+#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
+#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
+#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
+#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
 
-# Tu podać ścieżki do dysku:
-#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
-#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
-#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
-#file_extract.extract_files('/media/sandalas/FCBC-D18E/')
+
+
+# Narzędzie do konwersji .doc -> .docx
+#doc_to_pdf_conv.conv_2_pdf('./analysis_dir/docs/')
+
+
+
+# Analiza plików .pdf
 pdf_analyse.get_info('./analysis_dir/docs/')
