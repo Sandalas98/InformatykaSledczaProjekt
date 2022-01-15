1d1f93e3f4a6715709f22c0ccbb9777680cc8594
Michal Bugno <michal@kali.kali.com>
2022-01-16 00:05:45 +0100

    Improve face detection to not save all found images Imprve face detection, call functions from main.pyy

--- face_detection.py
+++ face_detection.py
@@ -11,22 +11,23 @@ def face_detection(path):
             iterator += 1
             print(path+'/'+img)
             img = cv2.imread(path+'/'+img)
-    
+
             # Convert into grayscale
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            
+
             # Load the cascade
-            face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-            
+            #face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
             # Detect faces
             faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            
+
             # Draw rectangle around the faces and crop the faces
             for (x, y, w, h) in faces:
                 cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
                 faces = img[y:y + h, x:x + w]
                 cv2.imshow("face",faces)
                 cv2.imwrite('face.jpg', faces)
-                
+
             # Display the output
-            cv2.imwrite(path+f'/face_{iterator}', img)
+            if len(faces):
+                cv2.imwrite(path+f'/face_{iterator}.jpg', img)
--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import os
 
 
 # Rozpoznaje system i użytkowników
-#get_users.get_users('./mount')
+get_users.get_users('./mount')
 
 # Ekstrakcja plików :
 # Tu podać ścieżki do dysku (lub folderu z wypakowanym obrazem dyskowym):
-#file_extract.extract_files('./mount') #ścieżka do katalogu dysku
+file_extract.extract_files('./mount') #ścieżka do katalogu dysku
 
-# Narzędzie do konwersji .docx -> .pdf, nie działa, poprawić
-#doc_to_pdf_conv.conv_2_pdf('./analysis_dir/docs/')
+#Narzędzie do konwersji .docx -> .pdf, nie działa, poprawić
+doc_to_pdf_conv.conv_2_pdf('./analysis_dir/docs/')
 
 # Analiza plików .pdf
-#pdf_analyse.get_info('./analysis_dir/docs/')
+pdf_analyse.get_info('./analysis_dir/docs/')
 
 # Ekstrakcja twarzy:
-#face_detection.face_detection('./analysis_dir/images')
+face_detection.face_detection('./analysis_dir/images')
