ef0280a9204c2bca6ba562863c8a68651dcb979e
Michal Bugno <michal@kali.kali.com>
2022-01-16 01:36:03 +0100

    Fixed file extracting

--- file_extract.py
+++ file_extract.py
@@ -6,11 +6,11 @@ def extract_files(path):
     if path == '':
         path = './mount'
 
-    text_file_end = ['.txt', '.doc', '.pdf']
-    image_file_ending = ['.png', '.jpg', '.bmp', '.gif']
-    web_sites_ending = ['.html']
-    music_files_ending = ['.wav', '.mp3']
-    source_file_ending = ['.cpp', '.c', '.h', '.vba', '.exe', '.bin', '.EXE']
+    text_file_end = ['txt', 'doc', 'pdf', 'docx']
+    image_file_ending = ['png', 'jpg', 'bmp', 'gif']
+    web_sites_ending = ['html']
+    music_file_ending = ['wav', 'mp3']
+    source_file_ending = ['cpp', 'c', 'h', 'vba', 'exe', 'bin', 'EXE']
 
     # Usuwanie straego folderu wraz z plikami:
     try:
@@ -33,29 +33,28 @@ def extract_files(path):
     for root, dirs, files in os.walk(path):
         for file in files:
             # Jeśli dany plik:
-
+            file_extension = file.split('.')[-1]
             # jest tekstem
-            if file.endswith(text_file_end[0]) or file.endswith(text_file_end[1]) or file.endswith(text_file_end[2]):
+            if file_extension in text_file_end:
                 shutil.copyfile(src = f'{os.path.join(root, file)}', dst = f'./analysis_dir/docs/{file}')
 
             # jest zdjęciem:
-            elif (file.endswith(image_file_ending[0]) or file.endswith(image_file_ending[1]) or 
-            file.endswith(image_file_ending[2]) or file.endswith(image_file_ending[3])):
+            elif file_extension in image_file_ending:
                 shutil.copyfile(src = f'{os.path.join(root, file)}', dst = f'./analysis_dir/images/{file}')
 
             # jest stroną html
-            elif file.endswith(web_sites_ending[0]):
+            elif fileextension in web_sites_ending:
                 shutil.copyfile(src = f'{os.path.join(root, file)}', dst = f'./analysis_dir/web_sites/{file}')
 
             # jest muzyką
-            elif file.endswith(music_files_ending[0]) or file.endswith(music_files_ending[1]):
+            elif file_extension in music_file_ending:
                 shutil.copyfile(src = f'{os.path.join(root, file)}', dst = f'./analysis_dir/music/{file}')
 
 
             # jest plikiem źródłowym lub wykonywalnym:
-            elif (file.endswith(source_file_ending[0]) or file.endswith(source_file_ending[1]) or 
-                file.endswith(source_file_ending[2]) or file.endswith(source_file_ending[3]) or 
-                file.endswith(source_file_ending[4]) or file.endswith(source_file_ending[5]) or 
+            elif (file.endswith(source_file_ending[0]) or file.endswith(source_file_ending[1]) or
+                file.endswith(source_file_ending[2]) or file.endswith(source_file_ending[3]) or
+                file.endswith(source_file_ending[4]) or file.endswith(source_file_ending[5]) or
                 file.endswith(source_file_ending[6])):
                 shutil.copyfile(src = f'{os.path.join(root, file)}', dst = f'./analysis_dir/source/{file}')
 
